d13779b7ab61d61205131ace4ec4e3b8ffb56fed
Łukasz Jurczyk <zabszk@protonmail.ch>
2019-11-10 17:36:31 +0100

    Fix for non-ASCII characters

--- process.h
+++ process.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <Windows.h>
 
-__declspec(dllexport) DWORD StartProcess(const char* command, const char* type);
-__declspec(dllexport) DWORD StartProcessHidden(const char* command, const char* type);
+__declspec(dllexport) DWORD StartProcess(const wchar_t* command, const wchar_t* type);
+__declspec(dllexport) DWORD StartProcessHidden(const wchar_t* command, const wchar_t* type);
 __declspec(dllexport) int KillProcess(DWORD pid);
 __declspec(dllexport) LPTSTR GetCommandLineArgs(void);
